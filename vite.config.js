e5a7c322af99657e1c56144ef940cbca296131ba
Suyash Rathod <suyash.rathod@example.com>
2026-06-29 15:17:59 +0530

    Added full code and fixed image

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure it builds to dist
+    assetsDir: 'assets',
+  },
+  publicDir: 'public', // Ye explicit bata raha hai ki public folder use karna hai
 })
